a77c8c57f34b163b4c5ac99c647f0f3754b3c981
varunk08 <varunk08@users.noreply.github.com>
2017-09-29 03:56:18 -0700

    deterministic reaping

---
+++ exception_flow/deterministic.c
@@ -0,0 +1,39 @@
+#include "../csapp_code/include/csapp.h"
+#define N 6
+
+int main()
+{
+    int status, i;
+    pid_t pid[N], retpid;
+
+    /* Parent creates N children */
+    for (i = 0; i < N; ++i)
+    {
+        if ( (pid[i] = Fork()) == 0) /* child has 0 ret value */
+        {
+            exit (100 + i);
+        }
+    }
+
+    /* Parent reaps N children */
+    i = 0;
+    while ( (retpid = waitpid(pid[i++], &status, 0)) > 0)
+    {
+        if (WIFEXITED(status))
+        {
+            printf("child %d terminated normally with exit status = %d\n", retpid, WEXITSTATUS(status));
+        }
+        else
+        {
+            printf("child %d terminated abnormally\n", retpid);
+        }
+    }
+
+    /* The only normal termination is if there are no more children */
+    if (errno != ECHILD)
+    {
+        unix_error("waitpid error");
+    }
+
+    exit(0);
+}
