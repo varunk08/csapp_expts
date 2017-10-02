7399e87ce4117a40c5d088de0fe6c43dc6f17a52
varunk08 <varunk08@users.noreply.github.com>
2017-10-02 04:33:11 -0700

    snooze with sig int handler

--- exception_flow/snooze.c
+++ exception_flow/snooze.c
@@ -1,19 +1,37 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "../csapp_code/include/csapp.h"
+
+unsigned int secs;
+
+void sigint_handler(int sig)
+{
+    printf("Caught SIGINT\n");
+    return;
+    
+}
 
 unsigned int snooze(unsigned int secs)
 {
     unsigned int ret = sleep(secs);
 
     printf("Slept for %d of %d secs.\n", secs - ret, secs);
+    exit(0);
+    //printf("Slept for %d of %d secs.\n", secs - ret, secs);
 
 
-    return ret;
+//    return ret;
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-    int i = snooze(5);
+
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    {
+        unix_error("signal error\n");
+    }
+
+    secs = atoi(argv[1]);
+    printf("secs: %d\n", secs);
+    int i = snooze(secs);
     return 0;
 }
